d16bb22bd8a3d14ecb485392249ad03005ca9ae4
Loren Hicks <lorenhicks@gmail.com>
2024-10-31 00:35:48 -0400

    Implement Dynamic Polymorphic Behavior Across Heterogeneous Class Instances

--- main.py
+++ main.py
@@ -7,26 +7,16 @@ class Employee():
   def say_id(self):
     print("My id is {}.".format(self.id))
 
-class User:
-  def __init__(self, username, role="Customer"):
-    self.username = username
-    self.role = role
-
-  def say_user_info(self):
-    print("My username is {}".format(self.username))
-    print("My role is {}".format(self.role))
-
-# Write your code below
-class Admin(Employee, User):
-  def __init__(self):
-    super().__init__()
-    User.__init__(self, self.id, "Admin")
-
+class Admin(Employee):
   def say_id(self):
     super().say_id()
     print("I am an admin.")
 
-e1 = Employee()
-e2 = Employee()
-e3 = Admin()
-e3.say_user_info()
+class Manager(Admin):
+  def say_id(self):
+    super().say_id()
+    print("I am on duty.")
+
+meeting = [Employee(), Admin(), Manager()]
+for m in meeting:
+  m.say_id()
